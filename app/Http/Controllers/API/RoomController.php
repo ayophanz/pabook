06fe677839e39147b552cdf7110ac60140a9cc05
Jford <ayophanz18@gmail.com>
2019-12-01 09:11:56 +0800

    update

--- app/Http/Controllers/API/RoomController.php
+++ app/Http/Controllers/API/RoomController.php
@@ -238,14 +238,14 @@ class RoomController extends Controller
     private function owner($roomType=0) {
       $user = auth('api')->user();
       if($roomType==0) {
-        $hotel = Hotel::select('id')->where('status', 'verified')->where('owner_id', $user->id)->get()->toArray();
+        $hotel = Hotel::select('id')->where('status', 'verified')->where('owner_id', $user->id)->pluck('id')->toArray();
         if(empty($hotel)) {
           $userMeta = UserMeta::select('user_id')->where('value', $user->id)->first();
           if(!empty($userMeta))
-            $hotel = Hotel::select('id')->where('status', 'verified')->where('owner_id', $userMeta->user_id)->get()->toArray();
+            $hotel = Hotel::select('id')->where('status', 'verified')->where('owner_id', $userMeta->user_id)->pluck('id')->toArray();
         }
 
-        $roomType = RoomType::select('id')->whereIn('hotel_id', $hotel)->get()->toArray();
+        $roomType = RoomType::select('id')->whereIn('hotel_id', $hotel)->pluck('id')->toArray();
         foreach ($roomType as $key => $value) {
           unset($roomType[$key]['room_type_refer']);
         }
